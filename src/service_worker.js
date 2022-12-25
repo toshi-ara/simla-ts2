7f5ec71b0fc2c68ddd92c25a41126577dfc93c80
ARA Toshiaki <toshiaki.ara@gmail.com>
2022-12-25 13:44:40 +0900

    update service_worker.js

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,25 +1,62 @@
-const CACHE_NAME = 'simla-ts-caches';
+// https://github.com/kanatapple/service-worker
+'use strict';
+
+const CACHE_NAME = 'cache-simla-v1';
 const urlsToCache = [
     'index.html',
     './dist/main.js'
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting()
     event.waitUntil(
         caches
             .open(CACHE_NAME)
-            .then(cache => {
+            .then((cache) => {
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+
+    self.clients.claim();
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
 self.addEventListener('fetch', event =>  {
     event.respondWith(
         caches
-            .match(event.request)
-            .then(response => {
-                return response ? response : fetch(event.request);
-            })
+        .match(event.request)
+        .then((response) => {
+            if (response) { return response; }
+
+            let fetchRequest = event.request.clone();
+            return fetch(fetchRequest)
+                .then((response) => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                });
+        })
     );
 });
