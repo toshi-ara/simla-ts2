642986e039c7956e48a40ba6c575db4e95e63ea0
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2023-10-14 13:06:53 +0900

    rename

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     output: {
         path: path.join(__dirname, "dist"),
-        filename: "main.js",
+        filename: "sim_local_anesthetics.js",
         publicPath: "/dist/"
     },
 
